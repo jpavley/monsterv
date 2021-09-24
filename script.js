435965f36d3b903e11261cb0677f87def2ded1a5
John Pavley <john@pavley.com>
2021-09-23 23:41:30 -0400

    wrapped in load event listener

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-canvas.width = 500;
-canvas.height = 800;
+document.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 800;
 
-class Game {
-    constructor() {
-        this.enemies = [];
+    class Game {
+        constructor() {
+            this.enemies = [];
+        }
+        update() {
+
+        }
+        draw() {
+
+        }
+        #addNewEnemy() {
+
+        }
     }
-    update() {
+
+    class Enemy {
+        constructor() {
+
+        }
+        update() {
+
+        }
+        draw() {
+
+        }
+    }
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        requestAnimationFrame(animate);
 
     }
-    draw() {
-
-    }
-    #addNewEnemy() {
-
-    }
-}
-
-class Enemy {
-    constructor() {
-
-    }
-    update() {
-
-    }
-    draw() {
-
-    }
-}
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    requestAnimationFrame(animate);
-
-}
+});
