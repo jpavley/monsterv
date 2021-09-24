96e8f42a50aea42fb21afedd0389c4a22046d505
John Pavley <john@pavley.com>
2021-09-24 17:52:27 -0400

    animiate

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
@@ -21,20 +21,26 @@ document.addEventListener('load', function() {
 
     class Enemy {
         constructor() {
-
+            this.x = 100;
+            this.y = 100;
+            this.width = 100;
+            this.height = 100;
         }
         update() {
-
+            this.x -= 1;
         }
         draw() {
-
+            ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
 
-    function animate() {
+    let lastTime = 1;
+    function animate(timeStamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        //console.log(deltaTime);
         requestAnimationFrame(animate);
-
     }
+    animate(0);
 });
