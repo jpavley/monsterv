7a26d7fd89817fdbd9550411afcee203bfb180fa
John Pavley <john@pavley.com>
2021-09-24 23:15:37 -0400

    game object ref passed to enemy

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.height = 800;
 
     class Game {
-        constructor() {
+        constructor(ctx, width, height) {
+            this.ctx = ctx;
+            this.width = width;
+            this.height = height;
             this.enemies = [];
+            console.log(this.enemies);
         }
         update() {
-
+            this.enemies.forEach(object => object.update());
+            this.#addNewEnemy();
         }
         draw() {
-
+            this.enemies.forEach(object => object.draw());
         }
         #addNewEnemy() {
-
+            this.enemies.push(new Enemy(this));
         }
     }
 
     class Enemy {
-        constructor() {
-            this.x = 100;
-            this.y = 100;
+        constructor(game) {
+            this.game = game;
+            this.x = this.game.width;
+            this.y = this.game.height * Math.random();
             this.width = 100;
             this.height = 100;
         }
@@ -34,11 +40,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    const game = new Game(ctx, canvas.width, canvas.height);
     let lastTime = 1;
     function animate(timeStamp) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
+        game.update();
+        game.draw();
         //console.log(deltaTime);
         requestAnimationFrame(animate);
     }
