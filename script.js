25e6d4d1e05950cffebfc29bc3601e49d9dfc67b
John Pavley <john@pavley.com>
2021-09-26 22:11:03 -0400

    cut out a sprite from a sprite sheet with drawImage

--- script.js
+++ script.js
@@ -25,27 +25,23 @@ window.addEventListener('load', function() {
             }
 
             // update every frame (like every 16ms)
-            this.enemies.forEach(object => object.update());
+            this.enemies.forEach(object => object.update(deltaTime));
         }
         draw() {
             this.enemies.forEach(object => object.draw(this.ctx));
         }
         #addNewEnemy() {
-            this.enemies.push(new Enemy(this));
+            this.enemies.push(new Worm(this));
         }
     }
 
     class Enemy {
         constructor(game) {
             this.game = game;
-            this.x = this.game.width;
-            this.y = this.game.height * Math.random();
-            this.width = 100;
-            this.height = 100;
             this.markedForDeletion = false;
         }
-        update() {
-            this.x -= 1;
+        update(deltaTime) {
+            this.x -= this.vx * deltaTime;
 
             // remove enemy when it passes out of sight to the left
             if (this.x < 0 - this.width) {
@@ -53,12 +49,26 @@ window.addEventListener('load', function() {
             }
         }
         draw(ctx) {
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
     class Worm extends Enemy {
-        
+        constructor(game) {
+            super(game); // super before using "this"
+
+            this.spriteWidth = 229;
+            this.spriteHeight = 171;      
+
+            this.width = this.spriteWidth/2;
+            this.height = this.spriteHeight/2;
+
+            this.x = this.game.width;
+            this.y = this.game.height * Math.random();
+
+            this.image = worm;
+            this.vx = Math.random() * 0.1 + 0.1;
+        }
     }
 
     const game = new Game(ctx, canvas.width, canvas.height);
